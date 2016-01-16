96d785a6643a447bade282edd4d857c4a047eb32
sykoboss <snakiie.boss@gmail.com>
2016-01-15 21:36:07 -0700

    Create base.lua

---
+++ base.lua
@@ -0,0 +1,19 @@
+local base = {}
+
+base.x = 0
+base.y = 0
+base.health = 100
+
+function base:setPos(x, y)
+	base.x = x
+	base.y = y
+end
+
+function base:getPos()
+	return base.x, base.y;
+end
+
+function base:load()
+end
+
+return base;
